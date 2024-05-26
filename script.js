982195207be08515cbcfe66dc2d3e181ee3b459d
Bishoy Bassem <bishoybassemsamir@hotmail.com>
2024-05-26 20:25:01 +0300

    Finished working with the HTML, CSS and JavaScript files.

---
+++ script.js
@@ -0,0 +1,28 @@
+const userInput = document.getElementById("user-input");
+const convertBtn = document.getElementById("convert-btn");
+const resetBtn = document.getElementById("reset-btn");
+const lengthInput = document.getElementById("length-input");
+const volumeInput = document.getElementById("volume-input");
+const massInput = document.getElementById("mass-input");
+
+
+convertBtn.addEventListener("click", function () {
+
+    lengthInput.textContent = ""
+    volumeInput.textContent = ""
+    massInput.textContent = ""
+
+    lengthInput.textContent += `${userInput.value} meters = ${Number(userInput.value * 3.281).toFixed(3)} feet | ${userInput.value} feet = ${Number(userInput.value / 3.281).toFixed(3)} meters`
+
+    volumeInput.textContent += `${userInput.value} liters = ${Number(userInput.value * 0.264).toFixed(3)} gallons | ${userInput.value} gallons = ${Number(userInput.value / 0.264).toFixed(3)} liters`
+
+    massInput.textContent += `${userInput.value} kilos = ${Number(userInput.value * 2.204).toFixed(3)} pounds | ${userInput.value} pounds = ${Number(userInput.value / 2.204).toFixed(3)} kilos`
+})
+
+resetBtn.addEventListener("click", function() {
+    lengthInput.textContent = ""
+    volumeInput.textContent = ""
+    massInput.textContent = ""
+    userInput.value = ""
+})
+
